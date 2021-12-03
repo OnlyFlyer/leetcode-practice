85c4cb63703affd388cc5eb8681920fa8c6647ef
的卢 <wutao@cai-inc.com>
2021-12-03 16:04:59 +0800

    feat: code

--- code/[1]两数之和/index.js
+++ code/[1]两数之和/index.js
@@ -3,4 +3,12 @@
  * @param {object[]} s
  * @return {object[]}
  */
- var twoSum = function(arr = []) {};
+ var twoSum = function(nums, target) {
+  for(let i=0; i<nums.length-1; i++) {
+    for (let j=i+1; j<nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
