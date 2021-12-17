373cf2637d8e4a6d1a1da913406385fa4eee18a0
的卢 <wutao@cai-inc.com>
2021-12-17 09:49:24 +0800

    chore: 暂存

--- businessCode/100002/index.js
+++ businessCode/100002/index.js
@@ -64,4 +64,39 @@ const town = [
 */
 
 // code start
+function fn (inputArr, levelKeys = []) {
+  const result = []
+  function setSpanToLeaf(root, nextKey, lastObj = {}) {
+    if (!root || !root.length) return;
+    root.forEach((item) => {
+      if (!item[nextKey] || !item[nextKey].length) {
+        result.push({...lastObj, ...item, span: 1})
+        item.span = 1;
+        return;
+      }
+      const currIdx = levelKeys.findIndex(k => k === nextKey)
+      setSpanToLeaf(
+        item[nextKey],
+        levelKeys[currIdx + 1],
+        { ...lastObj, ...item }
+      )
+    })
+  }
+  setSpanToLeaf(inputArr, levelKeys[0]);
+  console.log(inputArr);
+  console.log('result:', result)
+  // function loop(root = [], nextKey) {
+  //   const cache = [];
+  //   return root.map()
+  // }
 
+  // function loop(cacheObj, item, currLevelKey, cacheRowSpan) {
+  //   if (currLevelKey === levelKeys[levelKeys.length - 1] || !item[currLevelKey] || item[currLevelKey].length) {
+  //     result.push({...cacheObj, rowSpan: cacheRowSpan + 1 });
+  //     return;
+  //   }
+  //   for (let i = 0; i < item.length; i++) {
+  //     const zxc = item[i];
+  //   }
+  // }
+}
