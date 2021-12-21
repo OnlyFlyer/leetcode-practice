94785c2a8b566f142b59df894801fea7e4f999c6
的卢 <wutao@cai-inc.com>
2021-12-21 10:17:27 +0800

    feat: loop tree

--- business/100003/index.js
+++ business/100003/index.js
@@ -98,4 +98,70 @@
 //     ],
 //   },
 // ];
+var input = [
+  {
+    id: 2,
+    no: '1',
+    pId: null,
+    score: 0,
+    children: [
+      { id: 3, no: '1.1', pId: 2, score: 4 },
+      { id: 4, no: '1.2', pId: 2, score: 5, children: [
+        { id: 5, no: '1.1.1', pId: 2, score: 6 },
+        { id: 6, no: '1.1.2', pId: 2, score: 7 },
+        { id: 7, no: '1.1.3', pId: 2, score: 8 },
+        { id: 8, no: '1.1.4', pId: 2, score: 9 },
+      ] },
+    ],
+  },
+  {
+    id: 12,
+    no: '2',
+    pId: null,
+    score: 0,
+    children: [
+      { id: 9, no: '2.1', pId: 5, score: 1 },
+      { id: 10, no: '2.2', pId: 5, score: 2 },
+      { id: 11, no: '2.2', pId: 5, score: 3, children: [
+        { id: 20, no: '2.2', pId: 5, score: 2 },
+        { id: 30, no: '2.2', pId: 5, score: 3 },
+        { id: 40, no: '2.2', pId: 5, score: 5 },
+      ] },
+    ],
+  },
+];
+/**
+ *
+ * @param {number} value
+ * @param {*} currentNode
+ * @param {*} tree
+ * @return {object} type Node { id:Number, no:String, pId:Number, score:Number }
+ *
+ */
+var handleInputChange = (value, currentNode = {}, tree) => {
+  const { id } = currentNode;
+  // parent = parent + value - score;
+  // 更新 node
+  function loop(root) {
+    const stack = [...root];
+    while (stack.length !== 0) {
+      const currNode = stack.pop();
+      if (currNode.children) {
+        // 非叶子结点
+        currNode.score = currNode.children.reduce((prev, { score } = {}) => prev + score, 0)
+        currNode.children.forEach((item) => {
+          stack.push(item);
+        })
+      } else {
+        // 叶子结点
+        if (currNode.id === id) {
+          currNode.score = value;
+        }
+      }
+    }
+  }
+  loop(tree);
+  console.log(tree);
+};
 
+handleInputChange(999, {id: 3, score: 4 }, input)
