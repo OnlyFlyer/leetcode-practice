9a8b51acb79b841e9f9e05dab934579067cc1c8b
OnlyFlyer <scofield90.email@gmail.com>
2022-02-11 17:48:30 +0800

    feat: 暂存

--- code/[215]数组中的第K个最大元素/index.js
+++ code/[215]数组中的第K个最大元素/index.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
  var findKthLargest = function(nums, k) {
+  // return nums.sort((a, b) => b - a)[k-1]
   const cpNums = nums.concat();
   for (let i = 0; i < cpNums.length; i++) {
     for (let j = 0; j < cpNums.length; j++) {
