45d3fb129ca9a014d31883b75bfba36552197924
OnlyFlyer <scofield90.email@gmail.com>
2022-02-11 10:14:46 +0800

    feat: 冒泡排序

--- code/[215]数组中的第K个最大元素/index.js
+++ code/[215]数组中的第K个最大元素/index.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
  var findKthLargest = function(nums, k) {
-  return nums.sort((a, b) => b - a)[k-1]
+  const cpNums = nums.concat();
+  for (let i = 0; i < cpNums.length; i++) {
+    for (let j = 0; j < cpNums.length; j++) {
+      if (cpNums[i] > cpNums[j]) {
+        const cacheNum = cpNums[i];
+        cpNums[i] = cpNums[j];
+        cpNums[j] = cacheNum;
+      }
+    }
+  }
+  return cpNums[k-1];
 };
