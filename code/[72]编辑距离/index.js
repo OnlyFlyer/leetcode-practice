3e410e6b10e763bdb2f817b98c6a6eab6dd08138
的卢 <wutao@cai-inc.com>
2022-07-27 14:16:32 +0800

    chore: 更新题目

---
+++ code/[72]编辑距离/index.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+ var minDistance = function(word1, word2) {
+  if (word1 === word2) return 0;
+
+};
+// @lc code=end
+
